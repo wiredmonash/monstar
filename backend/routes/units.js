abfc1721c40de7b1b8cee2b1e5c4f657a4a15c51
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2024-09-18 18:28:13 +1000

    small changes to APIs

--- backend/routes/units.js
+++ backend/routes/units.js
@@ -9,7 +9,7 @@ const Review = require('../models/review');
 const router = express.Router();
 
 /**
- * GET Get All Units
+ * ! GET Get All Units
  * 
  * Gets all units from the database. 
  * 
@@ -26,14 +26,14 @@ router.get('/', async function (req, res) {
         return res.status(200).json(units);
     }
     catch (error) {
-        // Handle general errors
+        // Handle general errors 500
         return res.status(500).json({ error: `An error occured while getting all Units: ${error.message}` });
     }
 });
 
 
 /**
- * POST Add Unit
+ * ! POST Create a Unit
  * 
  * Creates a new Unit and adds it to the database.
  * 
@@ -64,28 +64,29 @@ router.post('/create', async function (req, res) {
 
 
 /**
- * DELETE Remove a Unit
+ * ! DELETE Remove a Unit
  * 
  * Deletes a Unit from the database
  * 
  * @async
  * @returns {JSON} Responds with a success message in JSON
  * @throws {500} If an error occurs
+ * @throws {404} Unit not found error
  */
 router.delete('/delete/:unitcode', async function (req, res) {
     try {
-        const unit = await Unit.findOneAndDelete({
-            unitCode: req.params.unitcode
-        });
+        // Find and delete the unit
+        const unit = await Unit.findOneAndDelete({ unitCode: req.params.unitcode });
 
-        if (!unit) {
-            return res.status(500).json({error: "Unit not found"});
-        }
+        // If the unit is null, then we did not find it.
+        if (!unit)
+            return res.status(404).json({ error: "Unit not found"});
 
-        // Using code 200 instead of 204 so I can send success message
-        return res.status(200).json({message: "Unit successfully deleted"});
+        // Respond with success message 200
+        return res.status(200).json({ message: "Unit successfully deleted" });
     }
     catch (error) {
+        // Handle general errors.
         return res.status(500).json({
             error: `Error occured while deleting unit: ${error.message}`
         });
@@ -93,42 +94,44 @@ router.delete('/delete/:unitcode', async function (req, res) {
 });
 
 /**
- * PUT Update a Unit
+ * ! PUT Update a Unit
  *  
  * Updates Unit description and/or name
  * 
  * @async
- * @returns {JSON} Responds with status 200 and JSON of the updated unit
+ * @returns {JSON} Responds with status 200 and success message
  * @throws {404} If the Unit is not found
  * @throws {500} If some error occurs
  */
-router.put('/update/:unitCode', async function (req, res) {
+router.put('/update/:unitcode', async function (req, res) {
     try {
         // Finding the unit
-        const unit = await Unit.findOne({unitCode: req.params.unitCode})
+        const unit = await Unit.findOne({unitCode: req.params.unitcode})
 
-        if (!unit) {    // If the unit doesn't exist, return an error
+        // If the unit doesn't exist return 404
+        if (!unit) {
             return res.status(404).json({
                 error: "Unit not found!"
             })
         }
 
-        const result = await Unit.updateOne({   // Update the unit if it exists
-            unitCode: req.params.unitCode
-        },
+        // Update the Unit
+        await Unit.updateOne({   
+                unitCode: req.params.unitcode
+            },
             {
-                $set: {
-                    name: req.body.unit_name || unit.name,  // Fallback in case the data hasn't been provided
-                    description: req.body.unit_description || unit.description
-                }
-            })
-
-        return res.status(200).json({   // Success
-            message: "Unit updated successfully!"
+            $set: {
+                name: req.body.unit_name || unit.name,
+                description: req.body.unit_description || unit.description
+            }
         })
+
+        // Respond with 200 and success message
+        return res.status(200).json({ msg: `Successfully updated ${req.params.unitcode}` });
     }
     catch (error) {
-        return res.status(500).json({   // Error handling
+        // Handle general errors
+        return res.status(500).json({
             error: `An error occurred while updating the unit: ${error.message}`
         })
     }
