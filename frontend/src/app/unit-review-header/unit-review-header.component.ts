1aa30677551aa9b6c2b530caa156bcac3db6a9f3
Your Name <you@example.com>
2024-09-30 19:47:52 +1000

    unit review component ts edit

--- frontend/src/app/unit-review-header/unit-review-header.component.ts
+++ frontend/src/app/unit-review-header/unit-review-header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component'; 
+import { FooterComponent } from '../shared/components/footer/footer.component'; 
 
 @Component({
   selector: 'app-unit-review-header',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent], 
   templateUrl: './unit-review-header.component.html',
-  styleUrl: './unit-review-header.component.css'
+  styleUrls: ['./unit-review-header.component.css'] 
 })
 export class UnitReviewHeaderComponent {
 
