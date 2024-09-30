de5b2d16a4a416b776ffd43d5d98515203018281
Your Name <you@example.com>
2024-09-30 19:37:02 +1000

    Component changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Routes } from '@angular/router';
 // Component Imports
 import { HomeComponent } from './routes/home/home.component';
 import { UnitListComponent } from './routes/unit-list/unit-list.component';
+import { UnitReviewHeaderComponent } from './shared/components/unit-review-header/unit-review-header.component'; 
 
 export const routes: Routes = [
     // Homepage
-    { path: "homepage", component: HomeComponent},
+    { path: "homepage", component: HomeComponent },
     // Unit List 
-    { path: "unit-list", component: UnitListComponent},
+    { path: "unit-list", component: UnitListComponent },
+    // Unit Review Header
+    { path: "unit-review-header", component: UnitReviewHeaderComponent } // New route
 ];
