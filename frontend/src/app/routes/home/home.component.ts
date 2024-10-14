d79a19754e3f24ef2428e9504765589fedfd5fe5
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2024-10-14 17:05:49 +1100

    initialisation of primeng ui library (works alongside bootstrap) - added primeng to styles. Layered on top of bootstrap so they can work together. - due to the above change, had to move styles references from angular.json to styles.scss and created a new file for layers called layer.css in public/styles/ - added provideAnimations() to app.config.ts (so we can have cool animations) - changed HomeComponent bootstrap accordion to ngprime accordion (looks very nice 😁), also has animations - Setup write review pop up with primeng p-dialog component, this needs to be altered by @ricky,  see unit-review-header.component.html and unit-review-header.component.ts. This is shown when the user presses the 'Write a review' button.

--- frontend/src/app/routes/home/home.component.ts
+++ frontend/src/app/routes/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- frontend/src/app/routes/unit-list/unit-list.component.ts
+++ frontend/src/app/routes/unit-list/unit-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-unit-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './unit-list.component.html',
   styleUrl: './unit-list.component.scss'
 })
