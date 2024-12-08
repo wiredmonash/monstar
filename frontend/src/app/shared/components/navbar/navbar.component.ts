0cf2134a8865f71d33bff33d55c504664d413962
jenul-ferdinand <jenul15ferdinand@gmail.com>
2024-12-08 13:51:46 +1100

    primeng sidebar and more FAQ info in homepage

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 // Allows us to go to different routes
 import { RouterLink } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { Ripple, RippleModule } from 'primeng/ripple';
+import { Sidebar, SidebarModule } from 'primeng/sidebar';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    SidebarModule,
+    ButtonModule,
+    RippleModule,
+    StyleClassModule,
+    AvatarModule,
+  ],
+  providers: [
+    provideAnimations(),
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+
+  closeCallback(e: any): void {
+    this.sidebarRef.close(e);
+  }
+
+  sidebarVisible: boolean = false;
+
 
 }
