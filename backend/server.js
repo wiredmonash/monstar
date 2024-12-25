ae73c536f4c19722ee90b4d09ebeecfa5ab4422e
jenul-ferdinand <jenul15ferdinand@gmail.com>
2024-12-25 18:24:50 +1100

    vercel is being a pain in the ass, probably gonna go with gcp

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,6 @@ const AuthRouter = require('./routes/auth');
 
 // === Middleware ===
 app.use(express.static(path.join(__dirname, '../frontend/dist/frontend/browser')));
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../frontend/dist/frontend/browser/index.html'));
-})
 app.use(express.json({ limit: '50mb' }));                                       // Increased payload limit for JSON requests.
 app.use(express.urlencoded({ limit: '50mb', extended: true }));                 // Increased payload limit for URL-encoded requests.
 app.use(cookieParser());
@@ -36,9 +33,7 @@ app.use((obj, req, res, next) => {
 // === Connect to MongoDB ===
 const url = process.env.MONGODB_CONN_STRING;
 async function connect(url) { 
-    await mongoose.connect(url, {
-        serverSelectionTimeoutMS: 30000
-    }); 
+    await mongoose.connect(url); 
 }
 connect(url)
     .then(console.log('Connected to MongoDB Database'))
