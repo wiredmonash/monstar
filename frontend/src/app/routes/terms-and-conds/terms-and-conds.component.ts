117b87b787bf646bb7159dc3ea74cf4a578d842b
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2025-02-18 09:39:02 +1100

    terms and conditions page
    Frontend Changes:
    - Added terms and conditions page, used devsoc unilectives format for this and also referencing the Monash University Student Code of Conduct in this.
    - Created new entry in the sidebar to navigate to the terms and conditions page.

---
+++ frontend/src/app/routes/terms-and-conds/terms-and-conds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-conds',
+  standalone: true,
+  imports: [],
+  templateUrl: './terms-and-conds.component.html',
+  styleUrl: './terms-and-conds.component.scss'
+})
+export class TermsAndCondsComponent {
+
+}
