c93bd3d89baacd5ba3eeaffdde8e4ff708a62e01
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2025-07-02 21:47:04 +1000

    proper navigation to setu overview from unit overview

--- frontend/src/app/shared/components/setu-card/setu-card.component.ts
+++ frontend/src/app/shared/components/setu-card/setu-card.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 // PrimeNG modules
 import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule }    from 'primeng/button';
 import { ProgressBarModule } from 'primeng/progressbar';
+import { Unit } from '../../models/unit.model';
 
 @Component({
   selector: 'app-setu-card',
@@ -17,11 +19,22 @@ import { ProgressBarModule } from 'primeng/progressbar';
   styleUrl: './setu-card.component.scss'
 })
 export class SetuCardComponent {
+  // Take the Unit input from unit overview (to get unitcode)
+  @Input() unit?: Unit;
+
+  constructor (
+    private router: Router
+  ) {}
+
   // View SETU overview in a new tab
   // This is a placeholder URL, replace with the actual SETU overview link
   goToSetuOverview(event: MouseEvent) {
+    if (!this.unit) return;
+    const thisUnitCode = this.unit.unitCode;
+
     event.stopPropagation(); // prevent accordion toggle
-    window.open('https://www.google.com/', '_blank'); // or _self to open in same tab
+    // window.open('https://www.google.com/', '_blank'); // or _self to open in same tab
+    this.router.navigate(['setu', thisUnitCode]);
   }
 }
 
