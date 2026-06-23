da221f744fbf607df76f637d6e152604bac3d724
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-06-23 19:09:24 +1000

    add review domain exports

---
+++ backend/src/domains/academics/reviews/index.ts
@@ -0,0 +1,6 @@
+export { default as Review } from './review.model';
+export { default as ReviewRepository } from './review.repository';
+export { default as ReviewService } from './review.service';
+export { default as ReviewController } from './review.controller';
+export { default as reviewsV2Router } from './reviews.v2.routes';
+export type { IReview, IReviewLean } from './review.types';
