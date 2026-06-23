e2facd07fc5a861deed6dcb1e58ca9dfb283aac3
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-06-23 19:09:24 +1000

    add org logo domain exports

---
+++ backend/src/domains/recruitment/orgLogo/index.ts
@@ -0,0 +1,5 @@
+export { default as OrgLogo } from './orgLogo.model';
+export { default as OrgLogoRepository } from './orgLogo.repository';
+export { default as OrgLogoService } from './orgLogo.service';
+export { default as OrgLogoController } from './orgLogo.controller';
+export type { IOrgLogo } from './orgLogo.types';
